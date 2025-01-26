7974002d62be33e282a37b4bc32796be7723710d
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-01-26 21:17:43 +0530

    Update apply.groovy

--- src/org/combine-p11/terraform-CD/apply.groovy
+++ src/org/combine-p11/terraform-CD/apply.groovy
@@ -3,9 +3,9 @@ package org.combine_p11.terraform_CD
 def call(string path="") {
     stage('Terraform apply or destroy') {
         if (params.action == "apply") {
-            sh "terraform apply ${path}"
+            sh "terraform apply -auto-approve ${path}"
         } else if (params.action == "destroy") {
-            sh "terraform destroy ${path}"
+            sh "terraform destroy -auto-approve ${path}"
         } else {
             echo "Invalid action"
         }
